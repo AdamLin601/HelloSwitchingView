cdd974313ab5fe1fac691dbdb1b5f921ea9f56b1
AdamLin <79ah2043@gmail.com>
2018-06-29 19:57:04 +0800

    Initial Commit

---
+++ HelloSwitchingView/LightRedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LightRedViewController.swift
+//  HelloSwitchingView
+//
+//  Created by 林奕德 on 2018/3/29.
+//  Copyright © 2018年 AppsAdamLin. All rights reserved.
+//
+
+import UIKit
+
+class LightRedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
